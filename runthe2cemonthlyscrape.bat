4290278a99cbf16c1e93aec4186afee1ffe112c0
Leonard Treman <leonardtreman@gmail.com>
2020-05-16 21:54:05 -0400

    Add files via upload

---
+++ runthe2cemonthlyscrape.bat
@@ -0,0 +1,15 @@
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\Scrape Test A Theory 2\dist\Scrape_Test_A_Theory_2.jar"
+timeout /t 600 /nobreak
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\LeonardScraperLauncher\dist\LeonardScraperLauncher.jar"
+timeout /t 2700 /nobreak
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\testingforgatecutters\dist\testingforgatecutters.jar"
+timeout /t 120 /nobreak
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\DateInsertLauncherScraper\dist\DateInsertLauncherScraper.jar"
+timeout /t 1800 /nobreak
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\PlasticsSolutionsUSAScraperLauncher\dist\PlasticsSolutionsUSAScraperLauncher.jar"
+timeout /t 1600 /nobreak
+timeout /t 180/nobreak
+java -jar "C:\Users\tremanleo\Documents\NetBeansProjects\CleanUp The Garbage\dist\CleanUp_The_Garbage.jar"
+timeout /t 10 /nobreak
+start https://www.youtube.com/watch?v=8sgycukafqQ
+msg tremanleo The Scrape Is Done
